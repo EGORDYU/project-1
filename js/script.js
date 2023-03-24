aea115bdd666508b3c40768e283dbb5ea4355a6c
EGORDYU <egordyuzhev@gmail.com>
2023-03-23 18:44:36 -0600

    5 sec test

--- js/script.js
+++ js/script.js
@@ -321,7 +321,7 @@ const background3 = new Sprite({
 //calls animation
 //win function
 function winTimer(level){
-    let timeLeft = 30;
+    let timeLeft = 5;
     const interval = setInterval(() => {
       timeLeft--;
       countdown.innerText = `${timeLeft} seconds left`;
