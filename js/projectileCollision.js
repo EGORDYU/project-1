52a794285d623ced947d87774d8e0f091289e2ed
EGORDYU <egordyuzhev@gmail.com>
2023-03-24 13:48:59 -0600

    invincibility

--- js/projectileCollision.js
+++ js/projectileCollision.js
@@ -1,21 +1,28 @@
+let invulnerable = false;
+const INVULNERABILITY_DURATION = 1000; // 1 second
+
 function checkProjectileCollision(player) {
-    for (let i = 0; i < projectiles.length; i++) {
-      const projectile = projectiles[i];
-      // check for collision between the player and the projectile
-      if (projectile.x + projectile.width >= player.position.x && 
-        projectile.x <= player.position.x + player.width &&
-        projectile.y + projectile.height >= player.position.y && 
-        projectile.y <= player.position.y + player.height) {
-        // decrease the lives and update the lifeText
+  for (let i = 0; i < projectiles.length; i++) {
+    const projectile = projectiles[i];
+    // check for collision between the player and the projectile
+    if (projectile.x + projectile.width >= player.position.x && 
+      projectile.x <= player.position.x + player.width &&
+      projectile.y + projectile.height >= player.position.y && 
+      projectile.y <= player.position.y + player.height) {
+      // decrease the lives and update the lifeText
+      if (!invulnerable) {
         lives--;
-        
-        // remove the projectile from the projectiles array
+        invulnerable = true;
         projectiles.splice(i, 1);
-        i--;
-        
+      i--;
+        setTimeout(() => {
+          invulnerable = false;
+        }, INVULNERABILITY_DURATION);
+      }
+      // remove the projectile from the projectiles array
+      
     }
   }
-
 }
 
 
